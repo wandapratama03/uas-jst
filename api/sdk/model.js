722e9a3be9bee38628e53d1912d5bbded1ef6b9d
wandapratama03 <84654036+wandapratama03@users.noreply.github.com>
2021-07-11 13:08:27 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,17 +1,17 @@
 const tf = require('@tensorflow/tfjs-node');
 
 function normalized(data){ // i & r
-    x = (data[0] - 4.2064) / 17.32172345
-    y = (data[1] - -2.565) / 24.97975683
-    z = (data[2] - -289.072) / 20.3396711
+    x1 = (data[0] - 4.2064) / 17.32172345
+    x2 = (data[1] - -2.565) / 24.97975683
+    x3 = (data[2] - -289.072) / 20.3396711
     return [x, y, z]
 }
 
 function denormalized(data){
-    x = (data[0] * 552.6264) + 650.4795
-    y = (data[1] * 12153.8) + 10620.5615
-    z = (data[2] * 12153.8) + 10620.5615
-    return [x, y, z]
+    x1 = (data[0] * 552.6264) + 650.4795
+    x2 = (data[1] * 12153.8) + 10620.5615
+    x3 = (data[2] * 12153.8) + 10620.5615
+    return [x1, x2, x3]
 }
 
 
@@ -25,7 +25,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/anzala0807/week-15-/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/wandapratama03/uas-jst/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
